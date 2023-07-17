627d77acd43ee8853b640d5df7d415bacc21069b
Bach16 <gwyn56717@gmail.com>
2023-07-17 01:44:25 -0500

    create filters layout

--- src/app/components/catalog/fliters/Filters.jsx
+++ src/app/components/catalog/fliters/Filters.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from "react";
+import FileList from "./FiltersItem";
+import ReactSlider from "react-slider";
+
+const ArrayCategories = [
+  {
+    id: 1,
+    name: "categorias",
+    label: "Arreglos",
+  },
+  {
+    id: 2,
+    name: "categorias",
+    label: "Bombones",
+  },
+  {
+    id: 3,
+    name: "categorias",
+    label: "Casas",
+  },
+];
+const ArrayGenere = [
+  {
+    id: 1,
+    name: "genero",
+    label: "Hombre",
+  },
+  {
+    id: 2,
+    name: "genero",
+    label: "Mujer",
+  },
+  {
+    id: 3,
+    name: "genero",
+    label: "Niño",
+  },
+];
 
 const Filters = () => {
-  return (
-    <div>Filters</div>
-  )
-}
+    const MIN = 3
+    const MAX = 50
+  const [values, setValues] = useState([MIN,MAX]);
 
-export default Filters
+  console.log(values);
+
+  return (
+    <>
+      <div className="flex flex-col w-[500px]">
+        <h2>Categorias</h2>
+        {ArrayCategories.map((c) => (
+          <FileList key={c.id} id={c.id} label={c.label} name={c.name} />
+        ))}
+        <h2>Genero</h2>
+        {ArrayGenere.map((c) => (
+          <FileList key={c.id} id={c.id} label={c.label} name={c.name} />
+        ))}
+        <h2>Precio</h2>
+        <ReactSlider className="slider" onChange={setValues} value={values} min={MIN}  max={MAX}/>
+        <h1>{values[0]} - {values[1]}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Filters;
