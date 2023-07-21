7aee88e8383ad6985b91160159e2f0ea9e72726f
Bach16 <gwyn56717@gmail.com>
2023-07-21 00:38:05 -0500

    add categorie filter global state

--- src/app/components/catalog/fliters/Filters.jsx
+++ src/app/components/catalog/fliters/Filters.jsx
@@ -1,8 +1,17 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import FileList from "./FiltersItem";
 import ReactSlider from "react-slider";
+import "./filters.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearFilter,
+  ChangeGenereInput,
+  ChangeCategoryInput,
+  ChangePriceInput,
+  filterAll,
+} from "@/redux/features/filter-slice";
 
 const ArrayCategories = [
   {
@@ -39,27 +48,107 @@ const ArrayGenere = [
   },
 ];
 
-const Filters = () => {
-    const MIN = 3
-    const MAX = 50
-  const [values, setValues] = useState([MIN,MAX]);
+const ArrayProducts = [
+  {
+    id: 1,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Arreglos",
+  },
+  {
+    id: 2,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Arreglos",
+  },
+  {
+    id: 3,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Casas",
+  },
+  {
+    id: 4,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Bombones",
+  },
+  {
+    id: 5,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Bombones",
+  },
+  {
+    id: 6,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Casas",
+  },
+  {
+    id: 7,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Arreglos",
+  },
+  {
+    id: 8,
+    name: "Fresas y Masmellos",
+    image: "https://dulcina.org/imgs/producto9.png",
+    price: 16.99,
+    category: "Arreglos",
+  },
+];
 
-  console.log(values);
+const Filters = () => {
+  const dispatch = useDispatch();
+  const filtered = useSelector((state) => state.filterReducer.value.filtered);
+  console.log(filtered);
+
+  const onChange = (e) => {
+    dispatch(ChangeCategoryInput(e.target.value));
+    dispatch(filterAll(ArrayProducts));
+  };
+
+  const MIN = 3;
+  const MAX = 50;
+  const [values, setValues] = useState([MIN, MAX]);
 
   return (
     <>
       <div className="flex flex-col w-[500px]">
         <h2>Categorias</h2>
         {ArrayCategories.map((c) => (
-          <FileList key={c.id} id={c.id} label={c.label} name={c.name} />
+          <FileList
+            key={c.id}
+            id={c.id}
+            onChange={onChange}
+            label={c.label}
+            name={c.name}
+          />
         ))}
         <h2>Genero</h2>
         {ArrayGenere.map((c) => (
           <FileList key={c.id} id={c.id} label={c.label} name={c.name} />
         ))}
         <h2>Precio</h2>
-        <ReactSlider className="slider" onChange={setValues} value={values} min={MIN}  max={MAX}/>
-        <h1>{values[0]} - {values[1]}</h1>
+        <ReactSlider
+          className="slider"
+          onChange={setValues}
+          value={values}
+          min={MIN}
+          max={MAX}
+        />
+        <h1>
+          {values[0]} - {values[1]}
+        </h1>
       </div>
     </>
   );
