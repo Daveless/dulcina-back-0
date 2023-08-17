ee6ebf33a465fcbfb8a880d6435add76f6c2cd3c
Bach16 <gwyn56717@gmail.com>
2023-08-16 22:19:44 -0500

    Update page.js

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,4 +1,4 @@
-import { ButtonContainer, CallButton } from "@/app/components";
+import { ButtonContainer, CallButton, StockButtons } from "@/app/components";
 
 const Product = ({ params }) => {
   const { id } = params;
@@ -7,11 +7,13 @@ const listOfButtons = [
 ]
 
   return (
-    <div className="flex text-[#222222] h-[550px] pt-[35px]">
-      <div className="w-[50%] h-[100%]"></div>
+    <div className="flex text-[#222222] h-[580px] pt-[35px]">
+      <div className="flex w-[50%] h-[100%] justify-center">
+        <img src="https://dulcina.org/imgs/producto9.png"/>
+      </div>
       <div className="w-[50%] h-[100%] pr-[110px] pl-[135px] flex  flex-col justify-between">
         <div>
-          <h1 className="text-[30px] font-black">
+          <h1 className="text-[33px] font-black leading-tight">
             Buzo Puma Classics Oversized
           </h1>
           <div>{/* icons */}</div>
@@ -26,6 +28,7 @@ const listOfButtons = [
           <ButtonContainer listOfButtons={listOfButtons}/>
         </div>
         {/* stock buttons */}
+        <StockButtons/>
         <CallButton />
       </div>
     </div>
