a7732c4cf65400fe33f5a1acbd51732c4aa3a9a7
Bach16 <gwyn56717@gmail.com>
2023-08-18 01:40:11 -0500

    update barrel

---
+++ src/app/components/ui/index.js
@@ -0,0 +1,4 @@
+export { default as ImageItem } from './ImageItem';
+export { default as LandingNavbar } from './LandingNavbar';
+export { default as Navbar } from './Navbar';
+export { default as Searchbar } from './Searchbar';
