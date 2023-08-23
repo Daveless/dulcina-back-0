ebe22207e9a27aafeabe412c54ad8acd3c528167
Bach16 <gwyn56717@gmail.com>
2023-08-23 15:37:36 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,21 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ReduxProvider } from "@/redux/provider";
+import { FooterContainer } from "./components";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Dulcina",
-  description: "Arreglos frutales",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#f2f2f2]`}>
+      <body className={`${inter.className}  bg-[#f2f2f2]`}>
         <ReduxProvider>
           {children}
         </ReduxProvider>
+        <footer>
+          <FooterContainer />
+        </footer>
       </body>
     </html>
   );
