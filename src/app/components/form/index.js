b92b8708bd573578523178e0da26c28ebd95c96c
Bach16 <gwyn56717@gmail.com>
2023-08-23 15:37:06 -0500

    Create index.js

---
+++ src/app/components/form/index.js
@@ -0,0 +1,4 @@
+export { default as FileInput } from './FileInput';
+export { default as FormContainer } from './FormContainer';
+export { default as FormInput } from './FormInput';
+export { default as SelectInput } from './SelectInput';
