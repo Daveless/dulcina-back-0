180a1089f543f3d38fde65c1950f32e73353434c
Bach16 <gwyn56717@gmail.com>
2023-08-24 03:01:20 -0500

    Update index.js

--- src/app/components/catalog/products/index.js
+++ src/app/components/catalog/products/index.js
@@ -1,3 +1,4 @@
 export { default as ListProducts } from './ListProducts';
 export { default as Product } from './Product';
+export { default as ProductDetails } from './ProductDetails';
 export { default as UxProducts } from './UxProducts';
