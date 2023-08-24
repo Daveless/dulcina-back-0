1a2cbb7e131e732a178e447312d02a2d3ed5c70c
Bach16 <gwyn56717@gmail.com>
2023-08-24 03:03:20 -0500

    Update page.js

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,53 +1,27 @@
 import {
-  ButtonContainer,
-  CallButton,
   Layout,
-  StockButtons,
+  ProductDetails
 } from "@/app/components";
 
 const Product = ({ params }) => {
   const { id } = params;
-  const listOfButtons = [
+  const options = [
     "Feliz Día",
     "Feliz Navidad",
     "Feliz Cumpleaños",
     "Personalizado",
   ];
 
+  const product = {
+    name:"Buzo Puma Classics Oversized",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    options:options,
+    price:"20.50"
+  }
+
   return (
     <Layout>
-      <div className="min-h-screen">
-
-      <div className="flex text-[#222222] h-[580px]  ">
-        <div className="flex w-[50%] h-[100%] justify-center">
-          <img src="https://dulcina.org/imgs/producto9.png" />
-        </div>
-        <div className="w-[50%] h-[100%] pr-[110px] pl-[100px] flex  flex-col justify-between">
-          <div>
-            <h1 className="text-[33px] font-black leading-tight">
-              Buzo Puma Classics Oversized
-            </h1>
-            <div>{/* icons */}</div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <div>
-            <h3 className="text-[25px] font-bold">Paleta</h3>
-            <ButtonContainer listOfButtons={listOfButtons} />
-          </div>
-          {/* stock buttons */}
-          <div className="flex justify-between">
-            <h3 className="text-[40px] font-medium">$20.50</h3>
-            <StockButtons />
-          </div>
-
-          <CallButton />
-        </div>
-      </div>
-    </div>
-
+      <ProductDetails product={product}/>
     </Layout>
   );
 };
