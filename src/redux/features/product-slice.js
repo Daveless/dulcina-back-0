ffca2e23540ec5f3c608692724b27ce15a610b44
Bach16 <gwyn56717@gmail.com>
2023-09-13 18:19:22 -0500

    Update product-slice.js

--- src/redux/features/product-slice.js
+++ src/redux/features/product-slice.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const { BACK_URL } = process.env
 
 const initialState = {
+  postedProduct:{},
   allProducts: [],
   error: "",
 };
@@ -14,15 +15,37 @@ export const fetchProducts = createAsyncThunk("product/fetchProducts", async () 
   );
   return res.data;
 });
+export const postProduct = createAsyncThunk("product/postProduct", async (body) => {
+  const product = {
+    name: body.name,
+    description: body.description,
+    highlight_date: body.highlighDate,
+    imageUrl: body.image,
+    price: body.price,
+    categoryId: 1
+  }
+  const res = await axios.post(
+    "https://dulcina-backend.onrender.com/products", product, {
+      headers:{Authorization:`Bearer ${body.token}`}
+    }
+  );
+  return res.data;
+});
 
 export const products = createSlice({
   name: "products",
-  initialState,
+  initialState,  
   extraReducers: (builder) => {
     builder.addCase(fetchProducts.fulfilled, (state, action) => {
       state.allProducts = action.payload
     });
     builder.addCase(fetchProducts.rejected, (state, action) => {
+      state.error = action.payload
+    });
+    builder.addCase(postProduct.fulfilled, (state, action) => {
+      state.postedProduct = action.payload
+    });
+    builder.addCase(postProduct.rejected, (state, action) => {
       state.error = action.payload
     });
   },
