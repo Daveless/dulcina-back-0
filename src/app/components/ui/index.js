15026dc7a9c249614f44f19fab09c0831aa98e2e
Bach16 <gwyn56717@gmail.com>
2023-09-18 18:35:06 -0500

    Update index.js

--- src/app/components/ui/index.js
+++ src/app/components/ui/index.js
@@ -3,5 +3,4 @@ export { default as ImageItem } from './ImageItem';
 export { default as LandingNavbar } from './LandingNavbar';
 export { default as Navbar } from './Navbar';
 export { default as Searchbar } from './Searchbar';
-export { default as Slider } from './Slider';
 export { default as Toast } from './Toast';
