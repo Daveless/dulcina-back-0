aa555189c94726798a5b24e6ac089d97f925914d
David <davidalrojas@gmail.com>
2023-09-27 18:28:25 -0500

    xd

---
+++ src/app/components/catalog/products/RelatedProducts.jsx
@@ -0,0 +1,41 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Product from "./Product";
+
+const RelatedProducts = ({ categoryId }) => {
+  const [relatedProducts, setRelatedProducts] = useState();
+  const shownProducts = relatedProducts?.slice(0, 5);
+
+  console.log("holaaaa", relatedProducts);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://dulcina-backend.onrender.com/products/relatedProducts/${categoryId}`
+      )
+      .then((res) => {
+        setRelatedProducts(res.data.products);
+      })
+      .catch((err) => console.log(err));
+  }, [categoryId]);
+
+  return (
+    <div className="w-[90%] mx-auto">
+      <h3 className="font-bold text-2xl my-6">Productos Relacionados</h3>
+      <div className="snap-mandatory snap-x flex flex-row gap-6 overflow-x-scroll ">
+        {shownProducts?.map((p) => (
+          <Product
+            className="snap-center"
+            key={p?.id}
+            id={p?.id}
+            name={p?.name}
+            image={p?.imageUrl}
+            price={p?.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default RelatedProducts;
