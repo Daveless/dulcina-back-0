0cd6abbe7ff85f32e7dfa7661b585d9b6d2f4be4
Bach16 <gwyn56717@gmail.com>
2023-09-27 21:21:55 -0500

    Update page.js

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,8 +1,4 @@
-"use client";
 import { Layout, ProductDetails } from "@/app/components";
-import axios from "axios";
-import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const Product = ({ params }) => {
   const { id } = params;
@@ -12,17 +8,11 @@ const Product = ({ params }) => {
     "Feliz Cumpleaños",
     "Personalizado",
   ];
-  const [product, setProduct] = useState();
 
-  useEffect(() => {
-    axios
-      .get(`https://dulcina-backend.onrender.com/products/${id}`)
-      .then((res) => setProduct(res.data.product))
-      .catch((err) => console.error(err));
-  }, []);
+  
   return (
     <Layout>
-      <ProductDetails product={product} />
+      <ProductDetails id={id} />
     </Layout>
   );
 };
