a1280eab2a7e4f0baf53b45a78c0e24db7ef17ed
Bach16 <gwyn56717@gmail.com>
2023-09-27 21:21:46 -0500

    Update Product.jsx

--- src/app/components/catalog/products/Product.jsx
+++ src/app/components/catalog/products/Product.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Product = ({ id, name, image, price, description }) => {
   return (
-    <Link href={`products/${id}`}>
+    <Link href={`/products/${id}`}>
         <div className="flex flex-col h-[375px] mb-[50px] justify-between">
           <div className=" flex justify-center  bg-[#EAEAEA] h-[300px] w-[235px] ">
             <img src={image} className="object-contain" />
