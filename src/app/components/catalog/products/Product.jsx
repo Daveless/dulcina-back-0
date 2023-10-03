b3193b2ce5f9ce109a449131a9d6198518939249
Bach16 <gwyn56717@gmail.com>
2023-10-03 17:30:35 -0500

    Update Product.jsx

--- src/app/components/catalog/products/Product.jsx
+++ src/app/components/catalog/products/Product.jsx
@@ -1,17 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Product = ({ id, name, image, price, description }) => {
   return (
     <Link href={`/products/${id}`}>
-        <div className="flex flex-col h-[375px] mb-[50px] justify-between">
-          <div className=" flex justify-center  bg-[#EAEAEA] h-[300px] w-[235px] ">
-            <img src={image} className="object-contain" />
-          </div>
-          <div>
-            <h2 className="text-[20px] font-medium">{name}</h2>
-            <p className="text-[18px] font-normal">{price}</p>
-          </div>
+      <div className="flex flex-col h-[375px] mb-[50px] justify-between">
+        <div className=" flex justify-center  bg-[#EAEAEA] h-[300px] w-[235px] ">
+          <Image
+            src={
+              "https://res.cloudinary.com/dccvupp4x/image/upload/f_auto,q_auto/v1/dulcina/products/cq0bnb62y2oumszfugm0"
+            }
+            width={235}
+            height={300}
+            sizes="33vw"
+            alt="producto"
+            objectFit="contain"
+          />
         </div>
+        <div>
+          <h2 className="text-[20px] font-medium">{name}</h2>
+          <p className="text-[18px] font-normal">{price}</p>
+        </div>
+      </div>
     </Link>
   );
 };
