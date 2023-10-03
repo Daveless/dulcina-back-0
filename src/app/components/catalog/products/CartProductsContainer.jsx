1a64de32bc828acdf91ab11e306025137dda9580
Bach16 <gwyn56717@gmail.com>
2023-10-03 17:30:28 -0500

    Create CartProductsContainer.jsx

---
+++ src/app/components/catalog/products/CartProductsContainer.jsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+import CartProduct from "./CartProduct";
+
+const CartProductsContainer = () => {
+  const [CartProducts, setCartProducts] = useState();
+  useEffect(() => {
+    setCartProducts(JSON.parse(localStorage.getItem("cartItems")) || []);
+  }, []);
+  console.log(CartProducts);
+  return (
+    <div>
+      {CartProducts?.length ? CartProducts?.map((p) => {
+        return <CartProduct key={p.id} setCartProducts={setCartProducts} id={p.id} name={p.name} price={p.price} imageUrl={p.imageUrl} />;
+      }): null}
+    </div>
+  );
+};
+
+export default CartProductsContainer;
