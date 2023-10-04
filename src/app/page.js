88e92dda3dd24992e91916a13ad9545a0258f08a
Bach16 <gwyn56717@gmail.com>
2023-10-03 20:35:38 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
-import { IgSection, ImageItem, LandingLayout} from "./components";
+import { IgSection, ImageItem, LandingLayout } from "./components";
 import Link from "next/link";
 import { Lobster } from "next/font/google";
 import Carrousel from "./components/landing/Carrousel";
@@ -27,60 +27,71 @@ export default function Home() {
               El detalle perfecto para alegrar cualquier ocasión especial
             </p>
 
-            <a href="#">
+            <Link href="/products">
               <button
                 type=""
                 className="bg-[#E60023] text-white px-10 sm:px-16 rounded-[0.3rem] text-[0.8rem] sm:text-[1.2rem] py-[0.5rem]"
               >
                 Ver Catálogo
               </button>
-            </a>
+            </Link>
           </div>
         </section>
       </LandingLayout>
 
-      <div className="flex-col justify-center items-center md:flex hidden">
-        <div className="mb-[15px] items-center flex w-[900px] justify-between">
-          <p className="text-[35px] text-[#272727] font-serif font-bold">
-            Categorias Populares
-          </p>
-          <Link href="/products">
-            <div className="flex w-[145px] items-center justify-between">
-              <p>Ver categorías</p>
-              <HiOutlineArrowLongRight size={"30px"} />
-            </div>
-          </Link>
+      <div className="sm:h-[500px]">
+        <div className="sm:h-[25%] " />
+
+        <div className="flex-col justify-center items-center md:flex hidden">
+          <div className="mb-[15px] items-center flex w-[900px] justify-between">
+            <p className="text-[35px] text-[#272727] font-serif font-bold">
+              Categorias Populares
+            </p>
+            <Link href="/products">
+              <div className="flex w-[145px] items-center justify-between">
+                <p>Ver categorías</p>
+                <HiOutlineArrowLongRight size={"30px"} />
+              </div>
+            </Link>
+          </div>
+          <div className="md:flex hidden">
+            <ImageItem
+              name={"Bombones"}
+              h={"240px"}
+              w={"300px"}
+              img={
+                "https://res.cloudinary.com/dccvupp4x/image/upload/v1693177563/dulcina/landing/items/sofvdc6gi4ocxeu8plhi.jpg"
+              }
+            />
+            <ImageItem
+              name={"Fresas"}
+              h={"240px"}
+              w={"300px"}
+              img={
+                "https://res.cloudinary.com/dccvupp4x/image/upload/v1693177566/dulcina/landing/items/omf0sbtzszecdhni4vbl.jpg"
+              }
+            />
+            <ImageItem
+              name={"Casas"}
+              h={"240px"}
+              w={"300px"}
+              img={
+                "https://res.cloudinary.com/dccvupp4x/image/upload/v1693177570/dulcina/landing/items/mldjjjumtilsyhlmno68.jpg"
+              }
+            />
+          </div>
         </div>
-        <div className="md:flex hidden">
-          <ImageItem
-            name={"Bombones"}
-            h={"240px"}
-            w={"300px"}
-            img={
-              "https://res.cloudinary.com/dccvupp4x/image/upload/v1693177563/dulcina/landing/items/sofvdc6gi4ocxeu8plhi.jpg"
-            }
-          />
-          <ImageItem
-            name={"Fresas"}
-            h={"240px"}
-            w={"300px"}
-            img={
-              "https://res.cloudinary.com/dccvupp4x/image/upload/v1693177566/dulcina/landing/items/omf0sbtzszecdhni4vbl.jpg"
-            }
-          />
-          <ImageItem
-            name={"Casas"}
-            h={"240px"}
-            w={"300px"}
-            img={
-              "https://res.cloudinary.com/dccvupp4x/image/upload/v1693177570/dulcina/landing/items/mldjjjumtilsyhlmno68.jpg"
-            }
-          />
-        </div>        
       </div>
 
-      <Carrousel />
-      <InfoSlider />
+      <div className="sm:h-[700px]">
+        <div className="sm:h-[25%] " />
+
+        <Carrousel />
+      </div>
+      <div className="sm:h-[600px] ">
+        <div className="sm:h-[20%] " />
+        <InfoSlider />
+      </div>
       <IgSection />
     </>
   );
