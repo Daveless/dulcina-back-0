7ee79aa60e62a835d459c52a5330e05788264538
Bach16 <gwyn56717@gmail.com>
2023-10-03 20:35:24 -0500

    Create CarrouselSqueleton.jsx

---
+++ src/app/components/ui/Squeletons/CarrouselSqueleton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CarrouselSqueleton = () => {
+  return (
+    <div>
+      <div className='bg-[#ffffff] h-[300px] w-[235px]'/>
+        
+    </div>
+  )
+}
+
+export default CarrouselSqueleton
