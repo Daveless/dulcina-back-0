403324b7e158d565ebea2d35468f6b7fd4191392
Bach16 <gwyn56717@gmail.com>
2023-10-04 19:13:49 -0500

    Update page.js

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,34 +1,45 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { CartProductsContainer, Layout } from "../components";
 import { useEffect, useState } from "react";
+import convertToDecimal from "@/assets/toDecimal";
+import { useDispatch } from "react-redux";
+import { patchTimesSoldProduct } from "@/redux/features/product-slice";
+import { ChangeCartLength } from "@/redux/features/cart-slice";
 
 const CartPage = () => {
   const [cartProducts, setCartProducts] = useState();
-  const convertToDecimal = (num) => {
-    let convertedNumber = 0;
-    if (num?.toString().length == 3) {
-      convertedNumber = String(num)[0] + "." + String(num)[1] + String(num)[2];
-    } else {
-      convertedNumber =
-        String(num)[0] + String(num)[1] + "." + String(num)[2] + String(num)[3];
-    }
-    return convertedNumber;
-  };
+  const dispatch = useDispatch()
+
   useEffect(() => {
     setCartProducts(JSON.parse(localStorage.getItem("cartItems")) || []);
   }, []);
-  const onBuy = () =>{
-    cartProducts.map
-  }
-  const precioTotal = cartProducts?.reduce((total, producto) => total + producto.price, 0)
+  const onBuy = () => {
+    cartProducts.map(p=>{
+      dispatch(patchTimesSoldProduct(p.id))
+    })
+    localStorage.setItem("cartItems", JSON.stringify([]))
+    dispatch(ChangeCartLength(0))
+  };
+  const precioTotal = cartProducts?.reduce(
+    (total, producto) => total + producto.price,
+    0
+  );
   return (
     <Layout title={"Dulcina"}>
       <div className="min-h-screen">
         <CartProductsContainer />
-        <Link rel="noopener noreferrer" target="_blank" href={`https://api.whatsapp.com/send?phone=593978778213&text=Quisiera%20comprar:${cartProducts?.map(e=>{return `%20${e.name}`})}%0APrecio%20estimado:%20${convertToDecimal(precioTotal)}
-`}>
-          <button>comprar</button>
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href={`https://api.whatsapp.com/send?phone=593978778213&text=Quisiera%20comprar:${cartProducts?.map(
+            (e) => {
+              return `%20${e.name}`;
+            }
+          )}%0APrecio%20estimado:%20$${convertToDecimal(precioTotal)}
+`}
+        >
+          <button onClick={onBuy}>comprar</button>
         </Link>
       </div>
     </Layout>
