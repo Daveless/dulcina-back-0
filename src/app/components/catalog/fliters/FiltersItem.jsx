e69651f2b78a4419e610146d057331c4734ce631
Bach16 <gwyn56717@gmail.com>
2023-10-07 20:57:45 -0500

    Update FiltersItem.jsx

--- src/app/components/catalog/fliters/FiltersItem.jsx
+++ src/app/components/catalog/fliters/FiltersItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const FiltersItem = ({ name, label, onChange }) => {
+const FiltersItem = ({ name, label,value, onChange }) => {
   return (
     <>
       <div className="flex items-center mb-[5px]">
-        <input className="mr-[10px]" type="radio" value={label} onClick={onChange} name={name}></input>
+        <input className="mr-[10px]" type="radio" value={value} onClick={onChange} name={name}></input>
         <label>{label}</label>
       </div>
     </>
