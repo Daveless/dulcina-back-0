fdeb4600722c01a69e4b17b52bc8c935f325aae1
Bach16 <gwyn56717@gmail.com>
2023-10-07 20:57:40 -0500

    Update Filters.jsx

--- src/app/components/catalog/fliters/Filters.jsx
+++ src/app/components/catalog/fliters/Filters.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FiltersItem from "./FiltersItem";
 import ReactSlider from "react-slider";
 import "./filters.css";
@@ -12,136 +12,35 @@ import {
   ChangePriceInput,
   filterAll,
 } from "@/redux/features/filter-slice";
+import { fetchCategories } from "@/redux/features/category-slice";
+import { capitalize } from "@/assets";
 
-const ArrayCategories = [
-  {
-    id: 1,
-    name: "categorias",
-    label: "Arreglos",
-  },
-  {
-    id: 2,
-    name: "categorias",
-    label: "Bombones",
-  },
-  {
-    id: 3,
-    name: "categorias",
-    label: "Casas",
-  },
-];
-const ArrayFestivities = [
-  {
-    id: 1,
-    name: "festividad",
-    label: "Día de la mujer",
-  },
-  {
-    id: 2,
-    name: "festividad",
-    label: "Día del padre",
-  },
-  {
-    id: 3,
-    name: "festividad",
-    label: "Día de la niño",
-  },
-  {
-    id: 4,
-    name: "festividad",
-    label: "Día de la madre",
-  },
-  {
-    id: 5,
-    name: "festividad",
-    label: "Navidad",
-  },
-];
-
-const ArrayProducts = [
-  {
-    id: 1,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Arreglos",
-    genere: "Hombre",
-  },
-  {
-    id: 2,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Arreglos",
-    genere: "Niño",
-  },
-  {
-    id: 3,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Casas",
-    genere: "Hombre",
-  },
-  {
-    id: 4,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Bombones",
-    genere: "Niño",
-  },
-  {
-    id: 5,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Bombones",
-    genere: "Mujer",
-  },
-  {
-    id: 6,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Casas",
-    genere: "Mujer",
-  },
-  {
-    id: 7,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Arreglos",
-    genere: "Niño",
-  },
-  {
-    id: 8,
-    name: "Fresas y Masmellos",
-    image: "https://dulcina.org/imgs/producto9.png",
-    price: 16.99,
-    category: "Arreglos",
-    genere: "Hombre",
-  },
-];
 
 const Filters = () => {
   const dispatch = useDispatch();
   const filtered = useSelector((state) => state.filterReducer.value.filtered);
   const byPrice = useSelector((state) => state.filterReducer.value.byPrice);
-  //console.log(byPrice);
+  const allCategories = useSelector(
+    (state) => state.categoryReducer.allCategories.categories
+  );
+  const products = useSelector(
+    (state) => state.productsReducer.allProducts.products
+  );
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, []);
 
   const onChangeCategory = (e) => {
     dispatch(ChangeCategoryInput(e.target.value));
-    dispatch(filterAll(ArrayProducts));
+    dispatch(filterAll(products));
   };
   const onChangeGenere = (e) => {
     dispatch(ChangeGenereInput(e.target.value));
-    dispatch(filterAll(ArrayProducts));
+    dispatch(filterAll(products));
   };
   const onChangePrice = (e) => {
     dispatch(ChangePriceInput(e));
-    dispatch(filterAll(ArrayProducts));
+    dispatch(filterAll(products));
   };
 
   return (
@@ -172,13 +71,14 @@ const Filters = () => {
             style={{ borderBottom: "1.5px solid #CFCFCF" }}
           />
 
-          {ArrayCategories.map((c) => (
+          {allCategories?.map((c) => (
             <FiltersItem
               key={c.id}
               id={c.id}
               onChange={onChangeCategory}
-              label={c.label}
-              name={c.name}
+              name="category"
+              value={c.id}
+              label={capitalize(c.name)}
             />
           ))}
           <div className="flex justify-between items-center">
