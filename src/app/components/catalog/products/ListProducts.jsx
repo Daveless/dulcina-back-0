481707a63d5286339c90433da99b975b581ec702
Bach16 <gwyn56717@gmail.com>
2023-10-07 20:57:52 -0500

    Update ListProducts.jsx

--- src/app/components/catalog/products/ListProducts.jsx
+++ src/app/components/catalog/products/ListProducts.jsx
@@ -8,6 +8,10 @@ import { ProductSqueleton } from "../../ui/Squeletons";
 
 const ListProducts = () => {
   const dispatch = useDispatch();
+  const filtered = useSelector((state) => state.filterReducer.value.filtered);
+  const categoryInput = useSelector(
+    (state) => state.filterReducer.value.byCategory
+  );
   const products = useSelector(
     (state) => state.productsReducer.allProducts.products
   );
@@ -16,20 +20,32 @@ const ListProducts = () => {
     dispatch(fetchProducts());
   }, []);
 
-  console.log(products);
+  if (categoryInput && !filtered?.length) {
+    return <div>not found</div>;
+  } else
     return (
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] max-w-[950px]  w-[100%] justify-center ">
-        {products
-          ? products?.map((p) => (
-              <Product
-                key={p.id}
-                id={p.id}
-                name={p.name}
-                image={p.imageUrl}
-                price={p.price}
-              />
-            ))
-          : [1,2,3,4,5,6].map(p=><ProductSqueleton key={p}/>)}
+        {filtered?.length || products?.length
+          ? categoryInput
+            ? filtered?.map((p) => (
+                <Product
+                  key={p.id}
+                  id={p.id}
+                  name={p.name}
+                  image={p.imageUrl}
+                  price={p.price}
+                />
+              ))
+            : products?.map((p) => (
+                <Product
+                  key={p.id}
+                  id={p.id}
+                  name={p.name}
+                  image={p.imageUrl}
+                  price={p.price}
+                />
+              ))
+          : [1, 2, 3, 4, 5, 6].map((p) => <ProductSqueleton key={p} />)}
       </div>
     );
 };
