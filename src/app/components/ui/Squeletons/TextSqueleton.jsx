cf74901d0ef23bf8d32a44d6a74a5d39d5f07b95
Bach16 <gwyn56717@gmail.com>
2023-10-09 22:05:05 -0500

    Create TextSqueleton.jsx

---
+++ src/app/components/ui/Squeletons/TextSqueleton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TextSqueleton = () => {
+  return (
+    <div className="bg-[#e9e9e9] rounded-md h-[20px] w-[200px] mb-[10px]">
+    </div>
+  )
+}
+
+export default TextSqueleton
