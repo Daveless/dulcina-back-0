cfd78a38f2f2225ce3473a1fe1c80c014e773c49
Bach16 <gwyn56717@gmail.com>
2023-10-09 23:33:08 -0500

    Update ListProducts.jsx

--- src/app/components/catalog/products/ListProducts.jsx
+++ src/app/components/catalog/products/ListProducts.jsx
@@ -10,23 +10,26 @@ const ListProducts = () => {
   const dispatch = useDispatch();
   const filtered = useSelector((state) => state.filterReducer.value.filtered);
   const categoryInput = useSelector(
-    (state) => state.filterReducer.value.byCategory
+    (state) => state.filterReducer.value.byCategory.id
   );
   const products = useSelector(
     (state) => state.productsReducer.allProducts.products
   );
+  const byPrice = useSelector((state) => state.filterReducer.value.byPrice);
+
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
+  console.log((byPrice[0] == 3) && (byPrice[1] == 50));
 
-  if (categoryInput && !filtered?.length) {
+  if ((categoryInput && !filtered?.length) || (((byPrice[0] != 3) && (byPrice[1] != 50)) && !filtered?.length)) {
     return <div>not found</div>;
   } else
     return (
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] max-w-[950px]  w-[100%] justify-center ">
         {filtered?.length || products?.length
-          ? categoryInput
+          ? categoryInput || !((byPrice[0] == 3) && (byPrice[1] == 50))
             ? filtered?.map((p) => (
                 <Product
                   key={p.id}
