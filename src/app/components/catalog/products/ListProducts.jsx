7be8120b5226b5bedfa30a54653ce43b4fece0a7
Bach16 <gwyn56717@gmail.com>
2023-10-13 01:12:25 -0500

    Update ListProducts.jsx

--- src/app/components/catalog/products/ListProducts.jsx
+++ src/app/components/catalog/products/ListProducts.jsx
@@ -13,24 +13,33 @@ const ListProducts = () => {
     (state) => state.filterReducer.value.byCategory.id
   );
   const products = useSelector(
-    (state) => state.productsReducer.allProducts.products
+    (state) => state.productsReducer?.allProducts?.products
   );
   const byPrice = useSelector((state) => state.filterReducer.value.byPrice);
-
+  const sortInput = useSelector((state) => state.sortReducer.sortInput);
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
-  console.log((byPrice[0] == 3) && (byPrice[1] == 50));
+ 
+const sorting = (input) =>{
+  if (input == "Popularidad") {
+    return (a, b) => b.times_sold - a.times_sold
+  } else if (input == "Nuevos") {
+    return (a, b) => new Date(b.createdAt) - new Date(a.createdAt)    
+  }}
 
-  if ((categoryInput && !filtered?.length) || (((byPrice[0] != 3) && (byPrice[1] != 50)) && !filtered?.length)) {
+  if (
+    (categoryInput && !filtered?.length) ||
+    (byPrice[0] != 3 && byPrice[1] != 50 && !filtered?.length)
+  ) {
     return <div>not found</div>;
   } else
     return (
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] max-w-[950px]  w-[100%] justify-center ">
         {filtered?.length || products?.length
-          ? categoryInput || !((byPrice[0] == 3) && (byPrice[1] == 50))
-            ? filtered?.map((p) => (
+          ? categoryInput || !(byPrice[0] == 3 && byPrice[1] == 50)
+            ? filtered.slice().sort(sorting(sortInput))?.map((p) => (
                 <Product
                   key={p.id}
                   id={p.id}
@@ -39,7 +48,7 @@ const ListProducts = () => {
                   price={p.price}
                 />
               ))
-            : products?.map((p) => (
+            : products.slice().sort(sorting(sortInput))?.map((p) => (
                 <Product
                   key={p.id}
                   id={p.id}
