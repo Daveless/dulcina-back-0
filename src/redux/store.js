c6019a3108583b35261d910eda2b7fc0ecc5b36d
Bach16 <gwyn56717@gmail.com>
2023-10-13 01:21:15 -0500

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import productsReducer from "./features/product-slice"
 import cartReducer from "./features/cart-slice"
 import userReducer from "./features/user-slice"
 import categoryReducer from "./features/category-slice"
+import sortReducer from "./features/sort-slice"
 
 export const store = configureStore({
     reducer: {
@@ -11,7 +12,8 @@ export const store = configureStore({
         productsReducer,
         cartReducer,
         userReducer,
-        categoryReducer
+        categoryReducer,
+        sortReducer
     }
 })
 
