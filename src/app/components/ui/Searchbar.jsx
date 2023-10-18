33c1f5a9486cde4ff2b37504b664d0f89d71f390
Bach16 <gwyn56717@gmail.com>
2023-10-18 16:16:48 -0500

    Update Searchbar.jsx

--- src/app/components/ui/Searchbar.jsx
+++ src/app/components/ui/Searchbar.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+"use client";
+import { changeSearchInput } from "@/redux/features/search-slice";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
+import { useDispatch } from "react-redux";
 
 const Searchbar = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const rout = useRouter();
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+  const onSearch = (e) => {
+    e.preventDefault();
+    dispatch(changeSearchInput(input));
+    rout.push("/products");
+
+    setInput("");
+  };
   return (
-    <div className="flex pl-[20px] text-[#222222] pr-[20px] bg-[#f2f2f2] h-[35px] items-center justify-between rounded-3xl">
-      <input
-        className="bg-[#f2f2f2] focus:outline-none"
-        placeholder={`Buscar...`}
-      />
-      <button className="text-[#222222]">
-        <HiOutlineSearch />
-      </button>
-    </div>
+    <>
+      <form onSubmit={onSearch}>
+        <div className="flex pl-[20px] text-[#222222] pr-[20px] bg-[#f2f2f2] h-[35px] items-center justify-between rounded-3xl">
+          <input
+            className="bg-[#f2f2f2] focus:outline-none"
+            placeholder={`Buscar...`}
+            value={input}
+            onChange={onChange}
+          />
+          <button type="submit" className="text-[#222222]">
+            <HiOutlineSearch />
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
