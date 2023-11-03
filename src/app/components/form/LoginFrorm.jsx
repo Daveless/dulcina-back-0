a9f9852fe457b0bd441bf98b8e5493899373babe
David <davidalrojas@gmail.com>
2023-11-03 02:08:03 -0500

    another approach to fix bug

--- src/app/components/form/LoginFrorm.jsx
+++ src/app/components/form/LoginFrorm.jsx
@@ -1,13 +1,14 @@
 "use client";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState, useCallback } from "react";
-import { FormInput } from ".";
+import { FormContainer, FormInput } from ".";
 import { loginUser } from "@/redux/features/user-slice";
 import { useRouter } from "next/navigation";
 
 const LoginFrorm = () => {
   const dispatch = useDispatch();
-  const router = useRouter();
+  const [logginState, setLogginState] = useState("login");
+
   const role = useSelector((state) => state.userReducer.role);
   const [input, setInput] = useState({
     email: "",
@@ -16,12 +17,10 @@ const LoginFrorm = () => {
 
   const redir = async () => {
     const dis = await dispatch(loginUser(input));
-    if (dis.payload.user.role !== "admin") {
-      console.log("entró en el if");
-      console.log("el rol es " + dis.payload.user.role + "en el login");
-    } else {
-      console.log("el rol si era admin y redirecciona");
-      router.push("admin/form");
+    if (dis.payload?.user?.role == "admin") {
+      setLogginState("dashboard");
+    } else if (dis.payload?.user?.role == undefined) {
+      setLogginState("unlogged");
     }
   };
   const onChange = (e, inputName) => {
@@ -38,31 +37,45 @@ const LoginFrorm = () => {
     });
     redir();
   };
-  return (
-    <form onSubmit={onSubmit} className="w-[50%]">
-      <FormInput
-        input={input}
-        onChange={(e) => onChange(e, "email")}
-        inputName="email"
-        label={"Email"}
-        type={"text"}
-        placeholder={"ejemplo@ejemplo.com"}
-      />
-      <FormInput
-        input={input}
-        onChange={(e) => onChange(e, "password")}
-        inputName="password"
-        label={"Password"}
-        type={"password"}
-      />
-      <button
-        type="submit"
-        className="bg-[#E60023] font-bold text-[#ffffff] w-[100%] h-[55px] rounded-xl"
-      >
-        Login
-      </button>
-    </form>
-  );
+
+  switch (logginState) {
+    case "login":
+      return (
+        <form onSubmit={onSubmit} className="w-[50%]">
+          <FormInput
+            input={input}
+            onChange={(e) => onChange(e, "email")}
+            inputName="email"
+            label={"Email"}
+            type={"text"}
+            placeholder={"ejemplo@ejemplo.com"}
+          />
+          <FormInput
+            input={input}
+            onChange={(e) => onChange(e, "password")}
+            inputName="password"
+            label={"Password"}
+            type={"password"}
+          />
+          <button
+            type="submit"
+            className="bg-[#E60023] font-bold text-[#ffffff] w-[100%] h-[55px] rounded-xl"
+          >
+            Login
+          </button>
+        </form>
+      );
+
+    case "dashboard":
+      return <FormContainer />;
+    case "unlogged":
+      return (
+        <div>
+          <p>Incorrect email or password</p>
+          <button onClick={() => setLogginState("login")}>Ok</button>
+        </div>
+      );
+  }
 };
 
 export default LoginFrorm;
