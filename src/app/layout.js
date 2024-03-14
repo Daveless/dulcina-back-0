c4ccd8d82b9eee7b00ea1cc6c4d25cd82c69d812
Bach16 <gwyn56717@gmail.com>
2024-03-14 13:14:34 -0500

    add google analytics tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,29 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ReduxProvider } from "@/redux/provider";
 import { FooterContainer } from "./components";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-32V122543H"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-32V122543H');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className}  bg-[#f8f8f8]`}>
         <ReduxProvider>
           {children}
